a448976ef422a00a2492f98402ae15601ba7996b
BritoJV <eng.jvbrito@gmail.com>
2023-09-20 23:14:24 -0300

    aplicado clarifai-nodejs-grpc

--- controllers/image.js
+++ controllers/image.js
@@ -1,40 +1,46 @@
+///////////////////////////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
 
-const clarifaiRequestOptions = (imageURL) =>{
-    const PAT = 'a334ec27669d4da7bf225dfe3037d0c1';
-    const USER_ID = 'pih18w73x5wx';       
-    const APP_ID = 'test';
-    const IMAGE_URL = imageURL;
-    const raw = JSON.stringify({
-        "user_app_id": {
-            "user_id": USER_ID,
-            "app_id": APP_ID
-        },
-        "inputs": [
-            {
-                "data": {
-                    "image": {
-                        "url": IMAGE_URL
-                    }
-                }
-            }
-        ]
-    });
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
-        },
-        body: raw
-    };
-    return requestOptions;
-}
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+const stub = ClarifaiStub.grpc();
+
+// This will be used by every Clarifai endpoint call
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
 
 const handleApiCall = (req,res) =>{
-    const options = clarifaiRequestOptions(req.body.input);
-    fetch(`https://api.clarifai.com/v2/models/face-detection/outputs`, options)
-    .then((response) => response.json())
-    .then((response => res.json(response.outputs[0].data.regions)))
+    const IMAGE_URL = req.body.input;
+    stub.PostModelOutputs(
+        {
+            user_app_id: {
+                "user_id": USER_ID,
+                "app_id": APP_ID
+            },
+            model_id: MODEL_ID,
+            version_id: MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+            inputs: [
+                { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+            ]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                throw new Error(err);
+            }
+
+            if (response.status.code !== 10000) {
+                throw new Error("Post model outputs failed, status: " + response.status.description);
+            }
+
+            // Since we have one input, one output will exist here
+            const output = response.outputs[0].data.regions;
+
+            res.json(output)
+        }
+
+    );
 }
 
 const handleImage =  (req, res, db) => {
